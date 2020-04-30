cb7a7d4d372f3f3936c7a54da9fd3ed5742fb82a
chaanks <jarod8405@gmail.com>
2020-04-30 23:59:04 +0200

    add files duration

---
+++ utils.py
@@ -0,0 +1,18 @@
+import json
+
+
+def load_json(path):
+    with open(path, 'r') as file:
+        return json.load(file)
+
+def write_json(path, data):
+    with open(path, 'w') as f:
+        json.dump(data, f)
+
+def load_2_col(path):
+    data = {}
+    with open(path) as fp:
+        for line in fp:
+            col = line.strip().split(' ')
+            data[col[0]] = col[1]
+    return data
