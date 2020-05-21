ac74b43ebf7f223f099091fa583ff557db6e20aa
chaanks <jarod8405@gmail.com>
2020-05-21 17:51:09 +0200

    remove xavier init , add batch normalization

--- model.py
+++ model.py
@@ -30,9 +30,9 @@ class Net(pl.LightningModule):
 
         self.rnn = nn.GRU(260, 128, num_layers=2, dropout=0.5, bidirectional=False) #4000
         self.fc1 = nn.Linear(128 * 50, 64)
+        self.bn1 = nn.BatchNorm1d(num_features=64)
         self.dropout = nn.Dropout(p=0.2)
         self.fc4 = nn.Linear(64, 1)
-        #self.bn1 = nn.BatchNorm1d(num_features=64)
 
     def forward(self, x):
         #print(x.shape)
@@ -40,7 +40,7 @@ class Net(pl.LightningModule):
         lstm_out, _ = self.rnn(x.view(x.shape[1], x.shape[0], -1))
 
         #print(lstm_out.shape)
-        x = self.dropout(F.relu(self.fc1(lstm_out.view(lstm_out.shape[1], -1))))
+        x = self.dropout(F.relu(self.bn1(self.fc1(lstm_out.view(lstm_out.shape[1], -1)))))
         #x = F.relu(self.fc2(x))
         #x = F.relu(self.fc3(x))
         x = self.fc4(x)
@@ -48,14 +48,14 @@ class Net(pl.LightningModule):
         #prob = F.log_softmax(x, dim=1)
         return x
 
-    def prepare_data(self):
+    #def prepare_data(self):
         # stuff here is done once at the very beginning of training
         # before any distributed training starts
 
         # download stuff
         # save to disk
         # etc...
-        self.apply(weight_init)
+        #self.apply(weight_init)
 
     def train_dataloader(self):
         # data transforms
@@ -109,19 +109,19 @@ class Net(pl.LightningModule):
 
         logits = nn.Sigmoid()(logits)
         pred = torch.full(logits.shape, 0).cuda()
-        pred[logits > 0.4] = 1
+        pred[logits > 0.5] = 1
 
         correct_pred = pred.eq(y.view_as(pred)).sum().item()
         idpred = list(zip(idx.cpu(), pred.cpu(), y.cpu()))
 
-        return {'val_loss': loss, 'correct': correct_pred, 'id_pred': idpred, 'logits': logits.cpu(), 'y_true': y.cpu()}
+        return {'val_loss': loss, 'val_correct': correct_pred, 'val_id_pred': idpred, 'val_logits': logits.cpu(), 'val_y_true': y.cpu()}
 
     def validation_epoch_end(self, outputs):
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        sum_correct = sum([x['correct'] for x in outputs])
-        all_pred = np.array(list(chain(*[x['id_pred'] for x in outputs])))
-        logits = nn.Sigmoid()(torch.Tensor(list(chain(*[x['logits'] for x in outputs])))).reshape(-1, 1)
-        y_true = np.array(list(chain(*[x['y_true'] for x in outputs])))
+        sum_correct = sum([x['val_correct'] for x in outputs])
+        all_pred = np.array(list(chain(*[x['val_id_pred'] for x in outputs])))
+        logits = nn.Sigmoid()(torch.Tensor(list(chain(*[x['val_logits'] for x in outputs])))).reshape(-1, 1)
+        y_true = np.array(list(chain(*[x['val_y_true'] for x in outputs])))
 
         score, acc = compute_score(self.ds_val, all_pred)
         #compute_precision_recall(y_true, logits)
@@ -134,8 +134,8 @@ class Net(pl.LightningModule):
         
         f1 = f1_score(y_true, all_pred[:, 1], labels=np.unique(all_pred))
         # Precision Recall
-        self.logger.log_metrics({'pr': wandb.plots.precision_recall(y_true, logits, ["no_target", "target"])})
-        logs = {'eval_loss': avg_loss, 'val_acc': acc, 'f1_score': f1}
+        #self.logger.log_metrics({'pr': wandb.plots.precision_recall(y_true, logits, ["no_target", "target"])})
+        logs = {'val_avg_loss': avg_loss, 'val_acc': acc, 'val_f1_score': f1}
 
         return {'val_loss': avg_loss, 'log': logs}
 
@@ -144,9 +144,34 @@ class Net(pl.LightningModule):
         x, y, idx = batch
         logits = self(x)
         loss = nn.BCEWithLogitsLoss()(logits, y)
-        return {'test_loss': loss}
+
+        logits = nn.Sigmoid()(logits)
+        pred = torch.full(logits.shape, 0).cuda()
+        pred[logits > 0.5] = 1
+
+        correct_pred = pred.eq(y.view_as(pred)).sum().item()
+        idpred = list(zip(idx.cpu(), pred.cpu(), y.cpu()))
+
+        return {'test_loss': loss, 'test_correct': correct_pred, 'test_id_pred': idpred, 'test_logits': logits.cpu(), 'test_y_true': y.cpu()}
 
     def test_epoch_end(self, outputs):
         avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        logs = {'test_loss': avg_loss}
+        sum_correct = sum([x['test_correct'] for x in outputs])
+        all_pred = np.array(list(chain(*[x['test_id_pred'] for x in outputs])))
+        logits = nn.Sigmoid()(torch.Tensor(list(chain(*[x['test_logits'] for x in outputs])))).reshape(-1, 1)
+        y_true = np.array(list(chain(*[x['test_y_true'] for x in outputs])))
+
+        score, acc = compute_score(self.ds_test, all_pred)
+        #compute_precision_recall(y_true, logits)
+
+        logits = np.hstack((np.zeros((logits.shape[0], 1)), logits))
+        for l in logits:
+            l[0] = 1 - l[1]
+
+        logger.info(score)
+        
+        f1 = f1_score(y_true, all_pred[:, 1], labels=np.unique(all_pred))
+        # Precision Recall
+        self.logger.log_metrics({'pr': wandb.plots.precision_recall(y_true, logits, ["no_target", "target"])})
+        logs = {'test_avg_loss': avg_loss, 'test_acc': acc, 'test_f1_score': f1}
         return {'test_loss': avg_loss, 'log': logs}
